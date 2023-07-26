60db00f4598d35fef755b3a0c25f5c95cb35ad44
Julia <girlfromrom@gmail.com>
2023-07-26 15:13:39 +0300

    app

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ const app = express();
 
 mongoose.connect(DB_URL);
 
-app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors);
+app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
